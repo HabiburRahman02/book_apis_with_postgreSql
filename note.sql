66fadac9a7add1b69430d3908f2c9c2c1b677044
Habibur Rahman <habibur88454@gmail.com>
2025-04-07 16:48:43 +0600

    get data using condition command

--- note.sql
+++ note.sql
@@ -8,21 +8,48 @@ CREATE TABLE person (
     first_name VARCHAR(50),
     last_name VARCHAR(50),
     gender VARCHAR(10),
-    date_of_birth TIMESTAMP
+    date_of_birth TIMESTAMP,
+    email VARCHAR(130)
 );
 
+CREATE TABLE person (
+    id BIGSERIAL NOT NULL PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50)  NOT NULL ,
+    gender VARCHAR(10)  NOT NULL,
+    date_of_birth TIMESTAMP NOT NULL,
+    email VARCHAR(130) NOT NULL
+);
+
+
+-- Table delete korar jonno  
+DROP TABLE person;
 
 -- person table a ki ki data ache dekhar jonno
 SELECT * FROM person;
 
 
 -- insert data in table
-INSERT INTO person(id,first_name,last_name,gender,date_of_birth)
+INSERT INTO person (first_name, last_name, gender, date_of_birth, email)
 VALUES
-(100, 'Rafi', 'Hasan', 'male', '2002-05-15 00:00:00'),
-(101,'Habibur', 'Rahman', 'male','2004-12-29 00:00:00');
+('Habibur', 'Rahman', 'Male', '12 march 2002','habib@gmail.com');
 
 
 -- existing kuno table er type change korar jonno
 ALTER TABLE person
 ALTER COLUMN gender TYPE VARCHAR(10);
+
+-- nidristo kuno table er data dekhar jonno
+SELECT first_name FROM person;
+
+
+-- order onusare data dekhano ASC, DESC
+SELECT * FROM person ORDER BY email;
+
+
+-- DATA duplicate hole ai command use korbo
+SELECT DISTINCT  country  FROM person ORDER BY country;
+
+
+-- condition onusare data get and multiple condition
+SELECT * FROM person  WHERE country = 'China' AND gender = 'Male';
